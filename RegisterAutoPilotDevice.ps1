ef16f372034c1a726673c75b062105d56a294c13
Mister Marko <admin@emkraan.com>
2025-10-03 16:56:30 -0400

    Removed Comments

--- RegisterAutoPilotDevice.ps1
+++ RegisterAutoPilotDevice.ps1
@@ -1,13 +1,5 @@
-﻿# Ensure script runs without user intervention
+﻿Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 
-# 1. Temporarily bypass execution policy for this session
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-
-# 2. Install the script silently (no confirmation prompts)
 Install-Script -Name Get-WindowsAutoPilotInfo -Force -Scope CurrentUser -Confirm:$false
 
-# 3. Ensure the script is available in the path
-$scriptPath = Join-Path $env:USERPROFILE "Documents\PowerShell\Scripts\Get-WindowsAutoPilotInfo.ps1"
-
-# Run the script online
-& $scriptPath -Online
+Get-WindowsAutoPilotInfo.ps1 -Online
