d6524f1d5d54a8c5c479550341b69d68debf2de7
cmarko89 <106281235+cmarko89@users.noreply.github.com>
2025-10-03 17:00:37 -0400

    Update RegisterAutoPilotDevice.ps1

--- RegisterAutoPilotDevice.ps1
+++ RegisterAutoPilotDevice.ps1
@@ -1,5 +1,3 @@
-﻿Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-
+﻿Set-ExecutionPolicy Bypass -Force -Scope Process
 Install-Script -Name Get-WindowsAutoPilotInfo -Force -Scope CurrentUser -Confirm:$false
-
 Get-WindowsAutoPilotInfo.ps1 -Online
